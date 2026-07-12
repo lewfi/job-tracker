678b265190e985d8732a168fa7bf45ee17e3bf85
lewfi <benjaminlien17@gmail.com>
2026-07-12 14:06:38 -0700

    fixing bs

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000"
+    baseURL: "https://job-tracker-production-23d0.up.railway.app"
 })
 
 export default api
